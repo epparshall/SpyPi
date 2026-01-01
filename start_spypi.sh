46deb0f346527442a63edcbc1bc19c658e8a88b8
Evan Parshall <epparshall@gmail>
2025-12-31 22:06:47 -0500

    No need for the sh

--- start_spypi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script is designed to run the SpyPi.py script on boot.
-# It navigates to the directory where this script is located, making it portable.
-cd "$(dirname "$0")"
-python SpyPi.py
